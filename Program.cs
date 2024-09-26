7891cd068bebf3ff5d9c46173f7571b3417daf52
Dheeraj kumar <dheerajkumar14.a@gmail.com>
2024-09-26 19:54:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, i, j = 1;
+            Console.WriteLine("Enter a number greater than zero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+             for (i = 1; i <= num1; i++)
+                {
+                    j = i * j;
+                }
+                Console.WriteLine($"Factorial of {num1} is: {j}");
+            }
+    }
+}
